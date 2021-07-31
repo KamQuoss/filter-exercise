01da96821e04c30e9567673d621c0924654bb8f1
KamQuoss <kamilaklosek@gmail.com>
2021-07-31 11:10:27 +0200

    readme added, material design added, generate cards instead of paragraphs

---
+++ README.md
@@ -0,0 +1,39 @@
+# Filter exercise
+
+This is solution of taks assignment during fullstack course at C_school. 
+
+## Overview
+
+### Task details
+- get data from web API ([todos](https://jsonplaceholder.typicode.com/todos))
+- display data on web page
+- build a form to filter data
+
+### Screenshot
+
+![](images/screenshot.png)
+
+### Links
+
+- Live Site URL: [here](https://kamquoss.github.io/filter-exercise/)
+
+## My process
+
+### Built with
+
+- Material Design (Materialize)
+- vanilla js
+
+### What I learned
+
+:construction_worker:
+[stage one](https://github.com/KamQuoss/filter-exercise/commit/4e0cf5e5cb9a38d5d1e68323265f91cad3891665)
+
+At first stage I've build simple fetch function and pure HTML form to manipulate visibility of data. At this point my knowledge about frontend was narrow, and my approach was to learn with small steps. After few months of commercial development I've decided to expand this little project. 
+
+Here comes stage two....
+
+
+## Author
+
+- LinkedIn - [Kamila Kłosek](https://www.linkedin.com/in/kamila-k%C5%82osek-b16b08a7/)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const filteredListCont = document.querySelector('#list-container'),
+const filteredListCont = document.querySelector('#results'),
     fetchURL = 'https://jsonplaceholder.typicode.com/todos',
     button = document.getElementById('getInfo');
 
@@ -23,7 +23,8 @@ let taskList,
     infoIdFrom = document.getElementById('id-from-error'),
     infoIdTo = document.getElementById('id-to-error'),
     // a to jest przefiltrowana lista, do wyświetlenia po kliku w button
-    filteredList;
+    filteredList,
+    userColors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green'];
 
 // tu pobierane są dane przy załadowaniu strony
 fetch(fetchURL)
@@ -31,82 +32,114 @@ fetch(fetchURL)
     .then(json => {
         taskList = json; // pobrane dane to task
         // tutaj będę pobierać wartości w jakich mogę się poruszać
+
         for (task in taskList) {
             userIdVal.add(taskList[task].userId)
         }
         userIdValTab = [...userIdVal];
         minIdVal = taskList[0].id;
         maxIdVal = taskList[taskList.length - 1].id;
+        filter();
 
-        // tu robię live filter na inputach, gdy zmienia się coś w inpucie to działa ta funkcja
-        userIdfilter.addEventListener('input', event => {
-            let val = parseInt(event.target.value);
-            if (!userIdVal.has(val)) {
-                infoUserId.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${userIdValTab[0]}-${userIdValTab[userIdValTab.length-1]}`;
-                userIdfilterVal = '';
-            } else {
-                userIdfilterVal = val;
-                infoUserId.innerHTML = `ok`;
-            }
-        });
-        idFromFilter.addEventListener('input', event => {
-            let val = parseInt(event.target.value);
-            if (val >= minIdVal && val < maxIdVal) {
-                infoIdFrom.innerHTML = `ok`;
-                idFromFilterVal = val;
-            } else {
-                infoIdFrom.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${minIdVal}-${maxIdVal-1}`;
-            }
-        });
-        idToFilter.addEventListener('input', event => {
-            let val = parseInt(event.target.value);
-            if (val > minIdVal && val <= maxIdVal) {
-                infoIdTo.innerHTML = ``;
-                idToFilterVal = val;
-            } else {
-                infoIdTo.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${minIdVal+1}-${maxIdVal}`;
-            }
-        });
-
-        // ta funkcja będzie wypisywać i filtrować na żądanie klienta
-        const filter = () => {
-            filteredList = taskList
-                .filter(task => {
-                    if (!userIdfilterVal) return true; // jeśli nie wpiszę nic to przechodzą wszystkie przez filtr
-                    else
-                        return task.userId == userIdfilterVal
-                })
-                .filter(task => {
-                    if (!idFromFilterVal) return true;
-                    else
-                        return (task.id >= idFromFilterVal)
-                })
-                .filter(task => {
-                    if (!idToFilterVal) return true;
-                    else
-                        return (task.id <= idToFilterVal)
-                })
-                .filter(task => {
-                    if (!titleFilter.value) return true;
-                    else
-                        return task.title.includes(titleFilter.value)
-                })
-                .filter(task => {
-                    return task.completed == completedFilter.checked
-                });
-                if(filteredList.length==0){
-                    filteredListCont.innerHTML = "Nie znalazłem wyników spełniającyh Twoje kryteria";
-                }
-                else{
-                    filteredListCont.innerHTML = "";
-                    for (task of filteredList) {
-                        let line = document.createElement("p");
-                        line.innerHTML = `User id: <b>${task.userId}</b>, id: <b>${task.id}</b>, title: <b>${task.title}</b>, checked: <b>${task.completed}</b>`;
-                        filteredListCont.appendChild(line)
-                    }
-                }
-            
-
-        }
-        button.addEventListener('click', filter)
     })
+// tu robię live filter na inputach, gdy zmienia się coś w inpucie to działa ta funkcja
+userIdfilter.addEventListener('input', event => {
+    let val = parseInt(event.target.value);
+    if (!userIdVal.has(val)) {
+        infoUserId.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${userIdValTab[0]}-${userIdValTab[userIdValTab.length - 1]}`;
+        userIdfilterVal = '';
+    } else {
+        userIdfilterVal = val;
+        infoUserId.innerHTML = `ok`;
+    }
+});
+idFromFilter.addEventListener('input', event => {
+    let val = parseInt(event.target.value);
+    if (val >= minIdVal && val < maxIdVal) {
+        infoIdFrom.innerHTML = `ok`;
+        idFromFilterVal = val;
+    } else {
+        infoIdFrom.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${minIdVal}-${maxIdVal - 1}`;
+    }
+});
+idToFilter.addEventListener('input', event => {
+    let val = parseInt(event.target.value);
+    if (val > minIdVal && val <= maxIdVal) {
+        infoIdTo.innerHTML = ``;
+        idToFilterVal = val;
+    } else {
+        infoIdTo.innerHTML = `Nieprawidłowa wartość, podaj liczbę z zakresu ${minIdVal + 1}-${maxIdVal}`;
+    }
+});
+
+// ta funkcja będzie wypisywać i filtrować na żądanie klienta
+
+const filter = () => {
+    filteredList = taskList
+        .filter(task => {
+            if (!userIdfilterVal) return true; // jeśli nie wpiszę nic to przechodzą wszystkie przez filtr
+            else
+                return task.userId == userIdfilterVal
+        })
+        .filter(task => {
+            if (!idFromFilterVal) return true;
+            else
+                return (task.id >= idFromFilterVal)
+        })
+        .filter(task => {
+            if (!idToFilterVal) return true;
+            else
+                return (task.id <= idToFilterVal)
+        })
+        .filter(task => {
+            if (!titleFilter.value) return true;
+            else
+                return task.title.includes(titleFilter.value)
+        })
+        .filter(task => {
+            return task.completed == completedFilter.checked
+        });
+    if (filteredList.length == 0) {
+        filteredListCont.innerHTML = "Nothing to show";
+    }
+    else {
+        filteredListCont.innerHTML = "";
+        for (task of filteredList) {
+            let card = document.createElement('div');
+            card.classList.add('col', 's12', 'm4', 'l3');
+            card.innerHTML =
+                `
+                <div class="card ${userColors[task.userId-1]} ${task.completed ? 'lighten-1' : 'darken-1'}">
+                    <div class="card-content ${task.completed ? 'black-text' : 'white-text'}">
+                        <span class="card-title info">
+                        <span><i class="card-title-icon material-icons">account_circle</i> ${task.userId}</span>                        
+                        <span>done: <i class="card-title-icon material-icons">${task.completed ? 'done' : 'clear'}</i></span>
+                        </span>
+                        <span class="card-title"><span><i class="card-title-icon material-icons">edit</i> ${task.id}</span> ${task.title}</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam provident, commodi eaque voluptatem
+                        omnis eligendi magnam ut saepe autem illum ipsam explicabo quis corporis quod et labore, doloribus
+                        reiciendis quo.</p>
+                    </div>
+                </div>
+                `;
+            filteredListCont.append(card)
+        }
+    }
+}
+button.addEventListener('click', filter);
+
+
+var slider = document.getElementById('test-slider');
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    step: 1,
+    orientation: 'horizontal', // 'horizontal' or 'vertical'
+    range: {
+        'min': 0,
+        'max': 100
+    },
+    // format: wNumb({
+    //     decimals: 0
+    // })
+});
